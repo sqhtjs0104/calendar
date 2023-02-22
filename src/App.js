fdc6ef7e29e650426419975e00d74c1fd2640d7e
sqhtjs0104 <sqhtjs0104@gmail.com>
2023-02-22 17:12:20 +0900

    Make default app structure & top div

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { memo } from 'react';
+import styled from 'styled-components';
 
-function App() {
+const Top = styled.div`
+  width: 100%;
+  padding: 10px;
+  background-color: #b6b6ff;
+  box-sizing: border-box;
+
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: end;
+
+  .top__nowYM {
+    width: 100%;
+    height: 50px;
+    background-color: tomato;
+    color: black;
+    height: 100%;
+
+    h1 {
+      padding: 0;
+      margin: 0;
+      margin-left: 20px;
+      display: block;
+      height: 100%;
+    }
+  }
+
+  button {
+    width: 30px;
+    height: 30px;
+    border: none;
+    border-radius: 10px;
+    color: #fff;
+    background: none;
+    font-size: 30px;
+
+    &:hover {
+      cursor: pointer;
+      scale: 1.1;
+    }
+  }
+`
+
+const App = memo(() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Top>
+        <button>&lt;</button>
+        <div className='top__nowYM'>
+          <h1>YYYY.MM</h1>
+        </div>
+        <button>&gt;</button>
+      </Top>
+    </>
   );
-}
+});
 
 export default App;
---
+++ src/components/Top.js
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+
+const Top = memo(() => {
+  return (
+    <>
+      
+    </>
+  );
+});
+
+export default Top;
